bb96ef48fd91f770f91330e7b7f3b794446e73a7
rafal <ra88fi@o2.pl>
2017-06-05 20:27:33 +0200

    fixed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-var text = "";
 for (i = 0; i < classButton.length; i++) {
+    var text = "";
     var classButton = document.getElementsByClassName("button");
     console.log(classButton);
     var i;
